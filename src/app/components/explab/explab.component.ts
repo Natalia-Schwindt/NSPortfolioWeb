fd0a3fd435dc4ad88da14953b7fb77aed8d59dec
Natalia Schwindt <naty8014@gmail.com>
2023-03-10 22:22:23 -0300

    nuevo commit

--- src/app/components/explab/explab.component.ts
+++ src/app/components/explab/explab.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/login.service';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-explab',
   templateUrl: './explab.component.html',
   styleUrls: ['./explab.component.scss']
 })
-export class ExplabComponent {
+export class ExplabComponent implements OnInit {
+  info:any;
+  Logged = false;
+  currentUser: any;
+  form: FormGroup;
 
+  constructor(private datos: LoginService, private fb: FormBuilder) {
+    this.form = this.fb.group({
+      Empresa: [''],
+      Puesto: [''],
+      Año: [''],
+      UrlDelLogo: ['']
+      });
+  }
+    ngOnInit(): void {
+      this.datos.ObtenerDatos('Explab').subscribe((data) => {
+        this.info = data;
+      });
+      this.form.patchValue(this.info), this.currentUser = this.datos.UsuarioAutenticado;
+    if (this.currentUser && this.currentUser.accessToken) {
+      this.Logged = true;
+    } else {
+      this.Logged = false;
+    };
+    
+}
 }
--- src/app/components/proyectos/proyectos.component.ts
+++ src/app/components/proyectos/proyectos.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/login.service';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.scss']
 })
-export class ProyectosComponent {
+export class ProyectosComponent implements OnInit {
+  info:any;
+  Logged = false;
+  currentUser: any;
+  form: FormGroup;
 
+  constructor(private datos: LoginService, private fb: FormBuilder) {
+       this.form = this.fb.group({
+      Nombre: [''],
+      Estado: [''],
+      Descripción: ['']
+      });
+  }
+    ngOnInit(): void {
+      this.datos.ObtenerDatos('Proyectos').subscribe((data) => {
+        this.info = data;
+      });
+      this.form.patchValue(this.info), this.currentUser = this.datos.UsuarioAutenticado;
+    if (this.currentUser && this.currentUser.accessToken) {
+      this.Logged = true;
+    } else {
+      this.Logged = false;
+    };
+    
+}
 }
